1f8070fa4c5d7e3fe7895758d3358d0b849d063a
Sushi Chanrai <sushi@pathworks.co>
2018-08-27 21:04:00 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,8 +1,8 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
 
   def change
-    add_column :students, :grade, :integer
-    add_column :students, :birthdate, :string
+    change_column(students, birthdate, datetime)
+    #add_column :students, :birthdate, :string
   end
 
 
