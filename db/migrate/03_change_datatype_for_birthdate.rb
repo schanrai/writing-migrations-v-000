0b37b71b80df0e66954f52ff74a2da9816854548
Sushi Chanrai <sushi@pathworks.co>
2018-08-27 21:05:39 +0000

    Done.

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,10 @@
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+
+  def change
+    change_column :students, :birthdate, :datetime
+    #add_column :students, :birthdate, :string
+  end
+
+
+
+end
